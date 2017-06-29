f1a63e4fea8369387f3b3f0385a5d198af3774da
gary <ggrumbley@ucsd.edu>
2017-06-28 21:37:21 -0700

    Add highlighting to code example

--- src/docs/ComponentPage.js
+++ src/docs/ComponentPage.js
@@ -1,10 +1,11 @@
-import React, { PropTypes } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import Example from './Example';
 import Props from './Props';
 
 const ComponentPage = ({component}) => {
   const {name, description, props, examples} = component;
-  // TODO
+
   return (
     <div className="componentpage">
       <h2>{name}</h2>
