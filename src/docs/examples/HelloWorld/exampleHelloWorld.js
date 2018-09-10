8aabb0b1e0f313c09ea8c4d857f9b1f7df70fbd7
Gary Grumbley <ggrumbley@trackforce.com>
2018-09-10 08:07:17 -0700

    Remove hello world component

--- src/docs/examples/HelloWorld/exampleHelloWorld.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import HelloWorld from 'ghg-react/HelloWorld';
-
-/** Custom message */
-export default function ExampleHelloWorld() {
-  return <HelloWorld message="Pluralsight viewers!" />
-}
